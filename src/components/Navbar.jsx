76c8db0b9522291e313793eb369990babb370816
NScoded <nileshsahu2005@gmail.com>
2026-02-15 19:00:50 +0530

    Add links for Certifications and Currently Learning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,8 @@ export default function Navbar() {
         <a href="#education" className="hover:text-cyan-400">Education</a>
         <a href="#skills" className="hover:text-cyan-400">Skills</a>
         <a href="#projects" className="hover:text-cyan-400">Projects</a>
+        <a href="#certifications" className="hover:text-cyan-400">Certifications</a>
+        <a href="#currentlylearning" className="hover:text-cyan-400">Exploraing</a>
         <a href="#contact" className="hover:text-cyan-400">Contact</a>
       </div>
     </nav>
